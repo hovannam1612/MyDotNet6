da77e80c86fd27157632eeca6771342015005b80
honam <hovannamedutt@gmail.com>
2022-03-23 14:33:46 +0700

    master 001

--- DemoNet6/MyModel.cs
+++ DemoNet6/MyModel.cs
@@ -6,5 +6,7 @@
         public string? Address { get; set; }
         public string? Description { get; set; }
         public string? Version1 { get; set; }
+        public string? Master1 { get; set; }
+        public string? Master2 { get; set; }
     }
 }
