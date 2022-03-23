27d72e34314ad46f2e30f05e9c854580d551afcb
honam <hovannamedutt@gmail.com>
2022-03-23 14:41:01 +0700

    commited

--- DemoNet6/MyModel.cs
+++ DemoNet6/MyModel.cs
@@ -6,7 +6,6 @@
         public string? Address { get; set; }
         public string? Description { get; set; }
         public string? Version1 { get; set; }
-        public string? Master1 { get; set; }
-        public string? Master2 { get; set; }
+        public string? Master22 { get; set; }
     }
 }
