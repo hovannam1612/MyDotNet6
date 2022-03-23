088aaa8337eb830478c3b8e7611d4539c92d6d67
honam <hovannamedutt@gmail.com>
2022-03-23 14:15:07 +0700

    first commit

---
+++ DemoNet6/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DemoNet6;
+
+var myModel = new MyModel();
+myModel.Name = "Ho Van Nam";
+myModel.Address = "Ha Noi";
+
+Console.WriteLine($"My name is {myModel.Name}, Address is {myModel.Address}");
